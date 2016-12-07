c4da4a3d8b585d6c4089eea4998496addc8189af
Sean Scott <sean.scott894@gmail.com>
2016-12-07 03:36:50 +0000

    Separated light stuff

---
+++ lightshow.py
@@ -0,0 +1,90 @@
+import time
+from neopixel import *
+
+# LED setup
+LED_COUNT       = 24            # Number of LED pixels
+LED_PIN         = 18            # GPIO pin connected to the pixels
+LED_FREQ_HZ     = 800000        # Frequency in Hertz (usually 800kHz)
+LED_DMA         = 5             # DMA channel for signal (usually 5)
+LED_BRIGHTNESS  = 10            # 0-255 dark to brightest (blinding)
+LED_INVERT      = False         # No inverters here...
+
+# Animations
+
+# Clear
+# Turns off all LEDs
+def display_clear():
+        for i in range(strip.numPixels()):
+                strip.setPixelColor(i, Color(0,0,0))
+        strip.show()
+
+# Add
+# Top half lit green
+def display_add():
+        for i in range(strip.numPixels()/2):
+                strip.setPixelColor(i, Color(255,0,0))
+        strip.show()
+
+# Remove
+# Bottom half lit red
+def display_remove():
+        for i in range(strip.numPixels()/2, strip.numPixels()):
+                strip.setPixelColor(i, Color(0,255,0))
+        strip.show()
+
+# Search
+# A chasing white wheel
+def display_search(wait_ms=50):
+	lit_leds = [0, 1, 2]
+	for i in range(strip.numPixels()):
+		display_clear()
+		for j in range(len(lit_leds)):
+			lit_leds[j] += 1
+			if (lit_leds[j] == strip.numPixels()):
+				lit_leds[j] = 0
+			strip.setPixelColor(lit_leds[j], Color(255,255,255))
+                strip.show()
+                time.sleep(wait_ms/1000.0)		
+
+# Success - Add
+# Three green blinks
+def display_success_add(wait_ms=250):
+	for i in range(3):
+
+		for j in range(strip.numPixels()):
+			strip.setPixelColor(j, Color(255,0,0))
+		strip.show()
+		time.sleep(wait_ms/1000.0)
+
+		display_clear()
+		time.sleep(wait_ms/1000.0)
+
+# Success - Remove
+# Three red blinks
+def display_success_remove(wait_ms=250):
+        for i in range(3):
+
+                for j in range(strip.numPixels()):
+                        strip.setPixelColor(j, Color(0,255,0))
+                strip.show()
+                time.sleep(wait_ms/1000.0)
+
+                display_clear()
+                time.sleep(wait_ms/1000.0)
+
+# Failure
+# Three yellow blinks
+def display_failure(wait_ms=250):
+        for i in range(3):
+
+                for j in range(strip.numPixels()):
+                        strip.setPixelColor(j, Color(255,255,0))
+                strip.show()
+                time.sleep(wait_ms/1000.0)
+
+		display_clear()
+                time.sleep(wait_ms/1000.0)
+
+# Initialization
+strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+strip.begin()
--- main.py
+++ main.py
@@ -1,20 +1,12 @@
 import time
 import RPi.GPIO as GPIO
-import zbar
+#import zbar
+import lightshow
 
-from SimpleCV import Color, Camera, Display
-from neopixel import *
+#from SimpleCV import Color, Camera, Display
 
 ADD_PIN		= 24		# PIN for add button
 REMOVE_PIN	= 23		# PIN for remove button
-
-# LED setup
-LED_COUNT 	= 24		# Number of LED pixels
-LED_PIN  	= 18		# GPIO pin connected to the pixels
-LED_FREQ_HZ	= 800000	# Frequency in Hertz (usually 800kHz)
-LED_DMA		= 5		# DMA channel for signal (usually 5)
-LED_BRIGHTNESS	= 10		# 0-255 dark to brightest (blinding)
-LED_INVERT	= False		# No inverters here...
 
 class Operation(object):
 	def __init__(self):
@@ -23,7 +15,7 @@ class Operation(object):
 def search(barcode):
 	# do something with the barcode
 	# while searching, do this...
-	display_search()
+	#display_search()
 	# if-else for displaying success/fail LEDs
 
 def scan():
@@ -37,42 +29,18 @@ def scan():
 		barcode = []
 		search(result)
 
-def display_clear():
-	for i in range(strip.numPixels()):
-		strip.setPixelColor(i, Color(0,0,0))
-	strip.show()
-
-def display_search(wait_ms=50):
-	for i in range(strip.numPixels()):	
-		strip.setPixelColor(i, Color(255,255,255))
-		if i > 2:
-			strip.setPixelColor(i - 2, Color(0,0,0))
-		strip.show()
-		time.sleep(wait_ms/1000.0)
-		
-
-def display_add():
-	for i in range(strip.numPixels()/2):
-		strip.setPixelColor(i, Color(255,0,0))
-	strip.show()		
-
-def display_remove():
-	for i in range(strip.numPixels()/2, strip.numPixels()):
-		strip.setPixelColor(i, Color(0,255,0))
-	strip.show()
-
 def on_press(channel):
 	if operation.waiting:
 		operation.waiting = False
-		display_clear()
+		#display_clear()
 	else:
 		operation.waiting = True
 		if channel == ADD_PIN:
 			print "add"
-			display_add()	
+			#display_add()	
 		elif channel == REMOVE_PIN:
 			print "remove"
-			display_remove()
+			#display_remove()
 		scan()
 
 # GPIO setup
@@ -87,13 +55,31 @@ GPIO.add_event_detect(23, GPIO.FALLING, callback=on_press, bouncetime=250)
 if __name__ == '__main__':
 	operation = Operation()
 
-	strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
-	strip.begin()
-
 	try:
-		print "Inventory 2.0"
 		while True:
-			pass
+			#Demo mode
+			lightshow.display_add()
+			time.sleep(1)
+			lightshow.display_clear()
+			lightshow.display_remove()
+			time.sleep(1)
+			lightshow.display_clear()
+			lightshow.display_search()
+			lightshow.display_search()
+			lightshow.display_search()			
+			lightshow.display_clear()
+			time.sleep(1)
+			lightshow.display_success_add()
+			lightshow.display_clear()
+			time.sleep(1)
+			lightshow.display_success_remove()
+			lightshow.display_clear()
+			time.sleep(1)
+			lightshow.display_failure()
+			lightshow.display_clear()
+			time.sleep(3)
+
+			#pass
 	except KeyboardInterrupt:
 		GPIO.cleanup()
 GPIO.cleanup()
