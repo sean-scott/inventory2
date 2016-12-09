11875ad9ac3cf699baf4b1353dfd3db74f500570
Sean Scott <sean.scott894@gmail.com>
2016-12-09 02:28:47 +0000

    rip

--- lookup.py
+++ lookup.py
@@ -2,7 +2,7 @@ import config # local file for storing the ACCESS_TOKEN
 import json
 import requests
 
-url = "http://www.searchupc.com/handlers/upcsearch.ashx?request_type=3&access_token=B4EDA630-C081-496C-B70C-2E7A44CAFCBA&upc="
+url = "http://www.searchupc.com/handlers/upcsearch.ashx?request_type=3&access_token=" + config.ACCESS_TOKEN + "&upc="
 
 def getProductJSON(barcode):
 	searchUrl = url + barcode
